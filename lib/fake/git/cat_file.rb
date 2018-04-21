e64148278fe4fce076b943d16e31eb3f117acc98
pauloancheta <paulo.ancheta@gmail.com>
2018-04-21 14:18:29 +1000

    add action to cat-file

--- lib/fake/git/cat_file.rb
+++ lib/fake/git/cat_file.rb
@@ -1,5 +1,28 @@
 class Fake::Git::CatFile
+  OBJ_PATH = ".fakegit/objects"
   def call(*args)
-    puts ARGV
+    file = file_path(args.first)
+    return file_size(file) if $OPTIONS[:cat_file_size]
+    return file_print(file) if $OPTIONS[:cat_file_print]
+    return file_type(file) if $OPTIONS[:cat_file_type]
+  end
+
+  private
+  def file_size(file)
+    result = `du -h #{file}`
+    puts result.split.first
+  end
+
+  def file_print(file)
+    content = File.read(file)
+    puts content
+  end
+
+  def file_type(file)
+    puts "blob"
+  end
+
+  def file_path(file)
+    "#{OBJ_PATH}/#{file[0..1]}/#{file[2..-1]}"
   end
 end
