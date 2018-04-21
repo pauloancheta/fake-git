c8e0cbd52e4744ec70bd7144ced43866ad3d9c1e
pauloancheta <paulo.ancheta@gmail.com>
2018-04-21 12:17:38 +1000

    introduce option parser and read from stdin

--- lib/fake/git.rb
+++ lib/fake/git.rb
@@ -11,16 +11,14 @@ $> echo 'hello world' | fakegit hash-object
 
 module Fake::Git
   def self.call(*args)
-    cmd, sub_cmd = args.first
+    cmd, sub_cmd = args
 
     if cmd.nil?
       puts HELP
       return
     end
 
-
-    classified_cmd = cmd.split("-").map(&:capitalize).join
-
+    classified_cmd = cmd.first.split("-").map(&:capitalize).join
     const_get("#{classified_cmd}").call(sub_cmd)
   end
 end
